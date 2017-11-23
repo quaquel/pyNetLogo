2c4e81448431ccb717375b4c39c8c7dea920dfa8
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2017-11-23 21:17:28 +0100

    further documentation fixes for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.append(os.path.abspath('../../'))
+sys.path.append(os.path.abspath('../../src/'))
 
 
 # -- General configuration -----------------------------------------------------
@@ -28,7 +28,7 @@ sys.path.append(os.path.abspath('../../'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.pngmath']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.imgmath']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -11,7 +11,7 @@ Contents:
 .. toctree::
    :maxdepth: 2
    
-   ../../_docs/netlogo.rst
+   ../../_docs/pyNetLogo.rst
 
 
 
