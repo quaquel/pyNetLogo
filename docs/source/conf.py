236f9acddeb5a5068c4c1d9aaa25193c6aeacec7
marcjaxa <M.Jaxa-Rozen@tudelft.nl>
2017-12-01 22:13:39 +0100

    Readthedocs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ sys.path.append(os.path.abspath('../../src/'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['nbsphinx', 'sphinx.ext.mathjax',
+extensions = ['sphinx.ext.mathjax',
 			  'sphinx.ext.autodoc', 'sphinx.ext.doctest','sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
