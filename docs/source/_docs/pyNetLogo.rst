564a76b27a8fc87f8262e7b5baeab34656942723
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2017-12-04 01:27:41 +0100

    reorganization for pip

--- docs/source/_docs/pyNetLogo.rst
+++ docs/source/_docs/pyNetLogo.rst
@@ -2,5 +2,5 @@
 :mod:`pyNetLogo`
 ****************
 
-.. automodule:: pyNetLogo.pyNetLogo
+.. automodule:: pyNetLogo
    :members:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.append(os.path.abspath('../../src/'))
+sys.path.append(os.path.abspath('../../pyNetLogo/'))
 
 
 # -- General configuration -----------------------------------------------------
