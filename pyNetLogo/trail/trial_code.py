8bb739259a1802b73d95cad2f4d7ee3b927b30ca
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2018-09-11 10:29:33 +0200

    updated test code

--- pyNetLogo/trail/trial_code.py
+++ pyNetLogo/trail/trial_code.py
@@ -3,44 +3,23 @@ Created on Dec 2, 2013
 
 @author: jhkwakkel
 '''
+from __future__ import print_function
+from pyNetLogo import NetLogoLink
+
 import os
-import jpype
 
-NETLOGO_HOME = r'/Applications/NetLogo 5.0.4'
-PYNETLOGO_HOME = r'/Domain/tudelft.net/Users/jhkwakkel/pyNetLogo/pyNetLogo/pyNetLogo'
+link = NetLogoLink()
 
-jars = [NETLOGO_HOME + r'/lib/scala-library.jar',
-        NETLOGO_HOME + r'/lib/asm-all-3.3.1.jar',
-        NETLOGO_HOME + r'/lib/picocontainer-2.13.6.jar',
-        NETLOGO_HOME + r'/lib/log4j-1.2.16.jar',
-        NETLOGO_HOME + r'/lib/jmf-2.1.1e.jar',
-        NETLOGO_HOME + r'/lib/pegdown-1.1.0.jar',
-        NETLOGO_HOME + r'/lib/parboiled-core-1.0.2.jar',
-        NETLOGO_HOME + r'/lib/parboiled-java-1.0.2.jar',
-        NETLOGO_HOME + r'/lib/mrjadapter-1.2.jar',
-        NETLOGO_HOME + r'/lib/jhotdraw-6.0b1.jar',
-        NETLOGO_HOME + r'/lib/quaqua-7.3.4.jar',
-        NETLOGO_HOME + r'/lib/swing-layout-7.3.4.jar',
-        NETLOGO_HOME + r'/lib/jogl-1.1.1.jar',
-        NETLOGO_HOME + r'/lib/gluegen-rt-1.1.1.jar',
-        NETLOGO_HOME + r'/NetLogo.jar',
-        PYNETLOGO_HOME + r'/java/netlogoLink.jar']
 
-jarpath = ":".join(jars) 
-jarpath = '-Djava.class.path={}'.format(jarpath)
-jvm_handle = jpype.getDefaultJVMPath()
+print("netlogo link instantiated")
+print(link.netlogo_home)
 
-print jvm_handle
+# assumes netlogo 6.0.3
+modelpath = os.path.join(link.netlogo_home, 
+         './models/Sample Models/Biology/Wolf Sheep Predation.nlogo')
+print(modelpath)
+print("does modelpath exist? " + str(os.path.exists(modelpath)))
 
-jpype.startJVM(jvm_handle, jarpath, "-Xmx1024m")
-jpype.java.lang.System.setProperty('user.dir', NETLOGO_HOME)
-jpype.java.lang.System.setProperty("java.awt.headless", "true");
+link.load_model(modelpath)
 
-print jpype.java.lang.System.getProperty("java.runtime.version")
-     
-link = jpype.JClass('netlogoLink.NetLogoLink')
-link = link(False, False)
-
-print "netlogo link instanciated"
-
-link.loadModel(r'/Domain/tudelft.net/Users/jhkwakkel/EMAworkbench/models/predatorPreyNetlogo/Wolf Sheep Predation.nlogo')
+print("model loaded successfully")
