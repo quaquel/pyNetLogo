af047413816f8a01445b737d02e57f2a83943663
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2018-11-22 07:55:34 +0100

    small readthedocs updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,7 +78,7 @@ release = '{}.{}.{}'.format(*version_info)
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['**.ipynb_checkpoints']
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
